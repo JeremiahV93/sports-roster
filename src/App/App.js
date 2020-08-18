d6d1643cb10fd07f2fb40b05eab8bbd1ad1faa24
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-08-17 21:29:46 -0500

    WIP

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
+import MyNavBar from '../components/MyNavbar';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
-        <button className="btn btn-info">I am a button</button>
+        <MyNavBar />
+        <h2>HI</h2>
       </div>
     );
   }
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MyNavBar extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+          <img src="../assets/dodgers-logo.png" width="30" height="30" alt="" />
+      </nav>
+    );
+  }
+}
+
+export default MyNavBar;
